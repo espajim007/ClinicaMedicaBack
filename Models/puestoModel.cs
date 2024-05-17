71a48ed42f701e8b43f386e6f77949f6a06c5df4
joseandresespa <jespanaj@miumg.edu.gt>
2024-05-17 09:34:23 -0600

    Errores de metodos

---
+++ Models/puestoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AnalisisIClinicaMedicaBack.Models
+{
+    public class puestoModel
+    {
+        public int? id_puesto_empleados { get; set; }
+        public string? nombre { get; set; }
+        public string? decripcion { get; set; }
+
+    }
+}
